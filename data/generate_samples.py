58b597500b96b4d1d3fbbdcb0105658353fa27ed
Laurens <laurens.devries@hotmail.com>
2024-02-02 15:41:31 +0100

    .

--- data/generate_samples.py
+++ data/generate_samples.py
@@ -3,12 +3,8 @@ import pandas as pd
 df = pd.read_csv('ukb_filtered_hariri_mh_upto69.csv', sep=' ')
 df_fc = pd.read_csv('ukb675871.bulk', sep=' ')
 
-# print(df['eid'].head(20))
-# print(df_fc.head(5))
+c1_values = list(set(df_fc.iloc[:, 0]))
 
-print(df.shape)
-print(df_fc.shape)
+df_fc_filtered = df[df['eid'].isin(c1_values)]
 
-print(df_fc.head(20))
-# for col in data.columns:
-#     print(col)
+print(df_fc_filtered.columns)
