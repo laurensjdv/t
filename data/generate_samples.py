1f25d84bbc2ba565268591490800dc409cb02128
Laurens <laurens.devries@hotmail.com>
2024-02-05 12:13:04 +0100

    fc vis

--- data/generate_samples.py
+++ data/generate_samples.py
@@ -1,10 +1,24 @@
 import pandas as pd
 
 df = pd.read_csv('ukb_filtered_hariri_mh_upto69.csv', sep=' ')
-df_fc = pd.read_csv('ukb675871.bulk', sep=' ')
+df_fc = pd.read_csv('ukb675871.bulk', sep=' ', names = ['eid', 'entry'])
 
+print(df_fc.head(5))
 c1_values = list(set(df_fc.iloc[:, 0]))
-
+print(list(df_fc.iloc[:, 0])[:10])
 df_fc_filtered = df[df['eid'].isin(c1_values)]
 
-print(df_fc_filtered.columns)
+# print(df_fc_filtered.shape)
+df_fc_filtered = df_fc_filtered[df_fc_filtered['Lifetime dep Smith'] != '-1']
+# print(df_fc_filtered.shape)
+
+eids = list(df_fc_filtered['eid'])
+
+df_fc = df_fc[df_fc['eid'].isin(eids)]
+# print(df_fc_filtered[df_fc_filtered['Lifetime dep Smith'] == -1])
+# print(df_fc_filtered['Lifetime dep stage'][:10])
+
+
+# print(df_fc[df_fc['entry'] == '25750_2_0'])
+
+df_fc.to_csv('ukb_filtered.bulk', sep=' ', index=False, header=False)
---
+++ load_fc.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = open('data/fetched/1000032_25750_2_0.txt', "r")
+
+fc = f.read()
+fc_l = fc.split()
+D = 21
+
+sq_fc = np.zeros((D,D))
+sq_fc[np.triu_indices(D,1)] = fc_l
+sq_fc += sq_fc.T
+
+print(sq_fc.shape)
+
+plt.imshow(sq_fc, cmap='hot')
+plt.colorbar()
+plt.show()
